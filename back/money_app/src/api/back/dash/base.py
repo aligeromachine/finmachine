171d5fba8e06eb543267164c166c69ca0b0db33c
aligero <aligero@list.ru>
2025-11-15 14:17:06 +0300

    v0.3.8

--- back/money_app/src/api/back/dash/base.py
+++ back/money_app/src/api/back/dash/base.py
@@ -6,5 +6,5 @@ logger = logging.getLogger(__name__)
 
 def base_info(item: DashboardMessage) -> dict:
     info: ReduceInfo = ReduceInfo.load_from_db(user_id=item.user_id)
-    data: dict = DashSignal(**info.model_dump()).to_dash()
+    data: dict = DashSignal(**info.model_dump(exclude={'calcWgRange', 'calcWgYears', 'totalSumCards'})).to_dash()
     return data
--- back/money_app/src/machine/func/model/sub.py
+++ back/money_app/src/machine/func/model/sub.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
-from pydantic import BaseModel
+from typing import Self
+from pydantic import BaseModel, model_validator
+from money.libs.math.exp import trim_decimal
 from money.libs.model.exp import BaseModelWithRawArray
 
 class Rng():
@@ -13,14 +15,34 @@ class DateRng(BaseModel):
     week: Decimal = Decimal(0)
     day: Decimal = Decimal(0)
 
+    @model_validator(mode='after')
+    def complete(self) -> Self:
+        self.year = trim_decimal(self.year)
+        self.month = trim_decimal(self.month)
+        self.week = trim_decimal(self.week)
+        self.day = trim_decimal(self.day)
+        return self
+
 class Capital(BaseModel):
     cash: Decimal = Decimal(0)
     cards: Decimal = Decimal(0)
     year: Decimal = Decimal(0)
 
+    @model_validator(mode='after')
+    def complete(self) -> Self:
+        self.cash = trim_decimal(self.cash)
+        self.cards = trim_decimal(self.cards)
+        self.year = trim_decimal(self.year)
+        return self
+
 class CardSelector(BaseModelWithRawArray):
     title: str = ""
     amount: Decimal = Decimal(0)
+
+    @model_validator(mode='after')
+    def complete(self) -> Self:
+        self.amount = trim_decimal(self.amount)
+        return self
 
 class CardsAgg(BaseModel):
     one: CardSelector = CardSelector()
